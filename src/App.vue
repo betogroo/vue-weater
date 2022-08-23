8886f9eff78561189a8161d156512868b95e1c9f
betogroo <luizhumberto@gmail.com>
2022-08-23 08:19:31 -0300

    chore: first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <v-app class="bg-indigo-lighten-4">
+    <AppBar />
+    <v-main>
+      <RouterView />
+    </v-main>
+  </v-app>
+</template>
---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vuetify from 'vite-plugin-vuetify'
+import path from 'path'
+import Components from 'unplugin-vue-components/vite'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    vuetify({ autoImport: true }),
+    Components({
+      dirs: [path.resolve(__dirname, 'src/components/app')],
+      dts: path.resolve(__dirname, '@types/components.d.ts')
+    })
+  ]
+})
